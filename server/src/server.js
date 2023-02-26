67c2eac3f8cd3b0faa77a3199f89a81c41b87978
Thomas Brockington <xtombrock1989@gmail.com>
2023-02-26 01:33:15 +0000

    save

--- server/src/server.js
+++ server/src/server.js
@@ -9,6 +9,7 @@ const morgan = require('morgan');
 
 app.disable('x-powered-by');
 
+
 // Add middleware
 app.use(cors());
 app.use(morgan('dev'));
@@ -18,7 +19,7 @@ app.use(express.static('public'))
 
 // Set the port and URl
 const PORT = process.env.PORT || 4000;
-const URL = process.env.URL || 'http://localhost:'
+const URL = process.env.URL || `http://localhost:`
 
 // Tell express to use your routers here
 const userRouter = require('./routes/users');
@@ -28,6 +29,7 @@ app.use('/users', userRouter);
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, '..', 'public')});
+    console.log('req', req.url);
 })
 
 // For all unknown requests 404 page returns
@@ -44,5 +46,5 @@ app.all('*', (req, res) => {
 
 // Start our API server
 app.listen(PORT, () => {
-    console.log(`\nServer is running on ${URL}${PORT} - this no longer consumes souls\n`);
+    console.log(`\nServer is running on ${URL}${PORT} \n This no longer consumes souls\n`);
 });
