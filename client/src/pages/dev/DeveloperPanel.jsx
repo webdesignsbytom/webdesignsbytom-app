23c3bfd97c093fbccdaf9355b980776f4edffa7c
Thomas Brockington <xtombrock1989@gmail.com>
2023-02-27 23:59:47 +0000

    save

--- client/src/pages/dev/DeveloperPanel.jsx
+++ client/src/pages/dev/DeveloperPanel.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 function DeveloperPanel() {
+  const [eventLog, setEventLog] = useState([])
+
+  useEffect(() => {
+    
+  }, [])
   return (
-    <div>DeveloperPanel</div>
-  )
+    <>
+      <div>
+        <h2>DeveloperPanel</h2>
+        <div>
+          <h4>Event Logs</h4>
+          <ul>
+            {eventLog.map((event, index) => {
+              return (
+                <li key={index}>
+                  <h6>event</h6>
+                  <p>event</p>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default DeveloperPanel
+export default DeveloperPanel;
