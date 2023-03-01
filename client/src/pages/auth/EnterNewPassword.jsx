768cca797188e386e7481a18acbf77cadf56b403
Thomas Brockington <xtombrock1989@gmail.com>
2023-03-01 03:16:35 +0000

    save

--- client/src/pages/auth/EnterNewPassword.jsx
+++ client/src/pages/auth/EnterNewPassword.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 // Axios
 import client from '../../users/utils/client';
 
