2d5282961a1aecb2a5c179c59babac319a27a105
xtombrock <xtombrock1989@gmail.com>
2023-03-03 14:07:21 +0000

    filter utils

---
+++ client/src/utils/FilterUtils.jsx
@@ -0,0 +1,52 @@
+export const devOptions = ['Select an Option', 'Most Recent', 'Oldest'];
+
+export const adminOptions = ['Select an Option', 'Most Recent', 'Oldest'];
+
+export const getByOptions = ['ID', 'Most Recent', 'Oldest'];
+
+export const getUserByOptions = ['ID', 'Email', 'Most Recent', 'Oldest'];
+
+export const getUsersByOptions = [
+  'Month',
+  'Year',
+  'Country',
+  'Open Projects',
+  'Closed Projects',
+];
+
+export const getEventByTypeOptions = [
+  'ADMIN',
+  'USER',
+  'VISITOR',
+  'DEVELOPER',
+  'ERROR',
+];
+
+export const getProjectsByOptions = [
+    'Completed',
+    'In Progress',
+    'User ID',
+    'User Email',
+    'Month',
+    'Year'
+]
+
+export const getReviewsByOptions = [
+    'ID',
+    'User ID',
+    'User Email',
+    'Email',
+    'Month',
+    'Year',
+    'Day'
+]
+
+export const getComplaintsByOptions = [
+    'ID',
+    'User ID',
+    'User Email',
+    'Email',
+    'Month',
+    'Year',
+    'Day'
+]
