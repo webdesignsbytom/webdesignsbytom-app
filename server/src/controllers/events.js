c57b6d2ebfcf09a56eaf2119b0a9c1c14630e61d
Thomas Brockington <xtombrock1989@gmail.com>
2023-03-09 19:53:52 +0000

    errors are back

--- server/src/controllers/events.js
+++ server/src/controllers/events.js
@@ -1,6 +1,7 @@
 // Emitters
 import { myEmitterErrors } from '../event/errorEvents.js';
 import { myEmitterEvents } from '../event/eventsLog.js';
+// Domain
 import { findAllEvents } from '../domain/events.js';
 // Response messages
 import { EVENT_MESSAGES, sendDataResponse, sendMessageResponse } from '../utils/responses.js';
@@ -13,8 +14,10 @@ import {
 
 export const getAllEvents = async (req, res) => {
   console.log('get all events');
+
   try {
     const foundEvents = await findAllEvents();
+    console.log('found events:', foundEvents);
 
     if (!foundEvents) {
       const notFound = new NotFoundEvent(
@@ -26,7 +29,8 @@ export const getAllEvents = async (req, res) => {
       return sendMessageResponse(res, notFound.code, notFound.message);
     }
 
-    myEmitterEvents.emit('get-all-events', req.user);
+
+    // // myEmitterEvents.emit('get-all-events', req.user);
     return sendDataResponse(res, 200, { events: foundEvents });
   } catch (err) {
     //
