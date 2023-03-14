93920b740baa8d1b2d5233a7e3cfe51c1b4ca166
xtombrock <xtombrock1989@gmail.com>
2023-03-14 05:01:34 +0000

    image

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -40,6 +40,7 @@ async function seed() {
         email: `email${i}@gmail${i}.com`,
         value: 5,
         content: 'Great',
+        image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80'
       },
     });
 
