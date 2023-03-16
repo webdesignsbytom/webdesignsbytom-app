d0059f175a68c7156fdcdc33389901a82d71fe81
xtombrock <xtombrock1989@gmail.com>
2023-03-16 10:15:34 +0000

    save

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -5,7 +5,6 @@ import ContactForm from './ContactForm';
 import Footer from '../../components/footer/Footer';
 
 function Contact() {
-
   return (
     <>
       <div className='bg-white dark:bg-black h-screen lg:grid lg:w-full lg:grid-rows-a1a lg:overflow-hidden'>
@@ -20,12 +19,59 @@ function Contact() {
               </div>
               <div className='sm:px-6'>
                 <p className='p-2 pb-6 md:mx-6 lg:mx-8'>
-                  If you need anything at all, just get in touch. If you need quotes or just to talk through some ideas. I am available to help. Either contact me throught the form provided, or use one the email address or phone number below. Myself or my staff are available to help all week. 
+                  If you need anything at all, just get in touch. If you need
+                  quotes or just to talk through some ideas. I am available to
+                  help. Either contact me throught the form provided, or use one
+                  the email address or phone number below. Myself or my staff
+                  are available to help all week.
                   <br />
-                  If you have designs made through our design tool we can access them and get and up to date view of what site design you have created.
+                  If you have designs made through our design tool we can access
+                  them and get and up to date view of what site design you have
+                  created.
                 </p>
               </div>
             </article>
+
+            <div className='contact__options'>
+              <article className='contact__option'>
+                {/* <HiOutlineMail className='contact__option-icon'/> */}
+                <h4>Email</h4>
+                <h5>tom@webdesignsbytom.com</h5>
+                <a
+                  href='mailto:tom@webdesignsbytom.com'
+                  rel='noreferrer'
+                  target='_blank'
+                >
+                  Send a message
+                </a>
+              </article>
+
+              <article className='contact__option'>
+                {/* <FaFacebookMessenger className='contact__option-icon'/> */}
+                <h4>Messenger</h4>
+                <h5>webdesignsbytom</h5>
+                <a
+                  href='http:facebook-subdomain'
+                  rel='noreferrer'
+                  target='_blank'
+                >
+                  Send a message
+                </a>
+              </article>
+
+              <article className='contact__option'>
+                {/* <ImWhatsapp className='contact__option-icon'/> */}
+                <h4>WhatsApp</h4>
+                <h5>Chat any time</h5>
+                <a
+                  href='https://api.whatsapp.com/send?phone+447841378347'
+                  rel='noreferrer'
+                  target='_blank'
+                >
+                  Send a message
+                </a>
+              </article>
+            </div>
           </section>
 
           {/* Inputs */}
