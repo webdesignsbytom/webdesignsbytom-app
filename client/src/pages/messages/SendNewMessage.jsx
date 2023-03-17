93255dc0aad9286afdf97d1983393062d2608435
xtombrock <xtombrock1989@gmail.com>
2023-03-17 22:03:45 +0000

    test box

--- client/src/pages/messages/SendNewMessage.jsx
+++ client/src/pages/messages/SendNewMessage.jsx
@@ -3,12 +3,13 @@ import Navbar from '../../components/nav/Navbar';
 
 function SendNewMessage() {
   return (
-    <section className='grid h-[calc(100vh-98px)] lg:h-screen lg:grid-rows-reg overflow-hidden border-2 border-black border-solid w-full'>
-      <div className='w-full'>
-        <Navbar />
-      </div>
-      SendNewMessage
-    </section>
+    <div className='bg-white dark:bg-black h-[calc(100vh-98px)] lg:h-screen w-full'>
+      <Navbar />
+
+      <main className='grid h-[calc(100vh-150px)] lg:h-screen lg:grid-rows-reg overflow-hidden border-2 border-black border-solid w-full'>
+        <section className='grid border-2 border-black border-solid bg-red-400'></section>
+      </main>
+    </div>
   );
 }
 
