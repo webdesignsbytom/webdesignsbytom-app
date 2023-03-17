9785a7b9b36a15650c01409c2948acb103520366
xtombrock <xtombrock1989@gmail.com>
2023-03-17 22:20:24 +0000

    save

--- client/src/pages/messages/SendNewMessage.jsx
+++ client/src/pages/messages/SendNewMessage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+// Components
 import Navbar from '../../components/nav/Navbar';
 
 function SendNewMessage() {
   return (
     <div className='bg-white dark:bg-black h-[calc(100vh-98px)] lg:h-screen w-full'>
       <Navbar />
-
-      <main className='grid h-[calc(100vh-170px)] my-2 px-2 lg:h-screen lg:grid-rows-reg overflow-hidden w-full'>
+      {/* Main */}
+      <main className='grid h-[calc(100vh-170px)] my-2 px-2 lg:grid-rows-reg overflow-hidden w-full'>
         <section className='grid border-2 border-black border-solid bg-red-400'></section>
       </main>
     </div>
