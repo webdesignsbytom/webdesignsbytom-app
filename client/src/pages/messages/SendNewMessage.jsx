4da001b6c24152d9a15d1f00b1932ec72ed77aa4
xtombrock <xtombrock1989@gmail.com>
2023-03-17 22:06:40 +0000

    test

--- client/src/pages/messages/SendNewMessage.jsx
+++ client/src/pages/messages/SendNewMessage.jsx
@@ -6,7 +6,7 @@ function SendNewMessage() {
     <div className='bg-white dark:bg-black h-[calc(100vh-98px)] lg:h-screen w-full'>
       <Navbar />
 
-      <main className='grid h-[calc(100vh-150px)] lg:h-screen lg:grid-rows-reg overflow-hidden border-2 border-black border-solid w-full'>
+      <main className='grid h-[calc(100vh-154px)] my-4 px-2 lg:h-screen lg:grid-rows-reg overflow-hidden w-full'>
         <section className='grid border-2 border-black border-solid bg-red-400'></section>
       </main>
     </div>
