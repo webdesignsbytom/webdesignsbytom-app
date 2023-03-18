7637b70557654246e3d805b448da4dcb41ae1f82
xtombrock <xtombrock1989@gmail.com>
2023-03-18 03:57:38 +0000

    save

--- client/src/pages/heros/HeroPage.jsx
+++ client/src/pages/heros/HeroPage.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 // Components
-import Navbar from '../../components/nav/Navbar'
+import Navbar from '../../components/nav/Navbar';
 function HeroPage() {
   return (
-    <div className='h-screen grid grid-rows-reg'>
+    <div className='bg-white lg:h-screen dark:bg-black lg:max-h-screen lg:overflow-hidden'>
       <Navbar />
-      <section className='grid h-full align-middle items-center justify-center'>
+      {/* Main */}
+      <section className='grid h-[calc(100vh-170px)] lg:h-full lg:pb-10 items-center justify-center'>
         <div className='w-full min-w-[300px] grid gap-2'>
-          <Link to='/developer'><button className='submit__button'>Hire Private</button></Link>
-          <Link to='/portfolio'><button className='alt__submit__button'>Hire Dev</button></Link>
+          <div className='text-2xl text-center'>
+            <h1>Web Design By Tom</h1>
+          </div>
+          <Link to='/developer'>
+            <button className='submit__button'>Hire Private</button>
+          </Link>
+          <Link to='/portfolio'>
+            <button className='alt__submit__button'>Hire Dev</button>
+          </Link>
         </div>
       </section>
     </div>
-  )
+  );
 }
 
-export default HeroPage
+export default HeroPage;
