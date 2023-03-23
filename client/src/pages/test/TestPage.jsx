9b94e0a1215996e945c8b07694d34ad059e58827
xtombrock <xtombrock1989@gmail.com>
2023-03-23 16:49:28 +0000

    save

--- client/src/pages/test/TestPage.jsx
+++ client/src/pages/test/TestPage.jsx
@@ -1,30 +1,27 @@
-import React from 'react';
-import Canvas from '../../components/animations/Canvas';
-import ParticleAnimation from '../../components/animations/ParticleAnimation';
+import React, { useEffect, useRef, useState } from 'react';
 
 function TestPage() {
-  const draw = (ctx, frameCount) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = '#000000';
-    ctx.beginPath();
-    ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
-    ctx.fill();
-  };
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+
+  useEffect(() => {
+
+    const canvas = canvasRef.current
+    canvas.width = window.innerWidth 
+    canvas.height = window.innerHeight 
+
+    const context = canvas.getContext("2d")
+
+    contextRef.current = context;
+  }, [])
+  
   return (
     <>
-      <section className='grid grid-rows-reg h-screen'>
-        <div>
-          <h1>Particles</h1>
-        </div>
-        <section className='grid grid-cols-2'>
-          <div className='bg-red-200 min-h-[600px]'>
-            <Canvas draw={draw} />
-          </div>
-          <div className='min-h-[600px]'>
-            <ParticleAnimation />
-          </div>
-        </section>
-      </section>
+      <canvas
+        ref={canvasRef}
+        // draw={draw}
+        className='absolute bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'
+      ></canvas>
     </>
   );
 }
---
+++ notes.md
@@ -0,0 +1,3 @@
+# Canvas
+
+1. control elements such as background colour with context - ctx
