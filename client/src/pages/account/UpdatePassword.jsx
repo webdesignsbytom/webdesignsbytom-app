d3963f83feb03d0b1cce58df7e5d5fa85a43f2ac
xtombrock <xtombrock1989@gmail.com>
2023-03-29 03:53:55 +0100

    save

--- client/src/pages/account/UpdatePassword.jsx
+++ client/src/pages/account/UpdatePassword.jsx
@@ -5,6 +5,10 @@ import Navbar from '../../components/nav/Navbar';
 import { SubmitButton } from '../../components/utils/SubmitButtons';
 import { ToggleContext } from '../../context/ToggleContext';
 import { statusResults } from '../../users/utils/utils';
+// Icons
+import OpenEye from '../../assets/svg/eye.svg';
+// Functions
+import { showPassword, showConfirmPassword } from '../../users/utils/PasswordReveal';
 
 function UpdatePassword() {
   const { toggleNavigation } = useContext(ToggleContext);
@@ -16,11 +20,40 @@ function UpdatePassword() {
   const [updateResponseMessage, setUpdateResponseMessage] =
     useState(statusResults);
 
+  const [fieldType, setFieldType] = useState('password');
+  const [eyeIcon, setEyeIcon] = useState(OpenEye);
+  const [fieldTypeConfirm, setFieldTypeConfirm] = useState('password');
+  const [eyeIconConfirm, setEyeIconConfirm] = useState(OpenEye);
+  const [formResponses, setFormResponses] = useState('');
+  const [hiddenPass, setHiddenPass] = useState('invisible h-4');
+  const [inputStyle, setInputStyle] = useState('standard__inputs');
+
   let navigate = useNavigate();
   const location = useLocation();
 
   console.log('NAVIGATION', navigate);
   console.log('location', location);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (newPasswordForm.password.length > 8) {
+      setFormResponses({
+        ...formResponses,
+        passwordLengthError: false,
+      });
+    } else {
+      setFormResponses({
+        ...formResponses,
+        passwordLengthError: true,
+      });
+    }
+
+    setNewPasswordForm({
+      ...newPasswordForm,
+      [name]: value,
+    });
+  };
 
   const updatePassword = () => {
     console.log('update');
@@ -30,28 +63,96 @@ function UpdatePassword() {
     <div className='bg-white dark:bg-black h-[calc(100vh-98px)] lg:h-screen w-full'>
       <Navbar />
       {!toggleNavigation && (
-        <div>
-          <div>
+        <main className='mx-2'>
+          <div className='my-4'>
             <h1>Password update</h1>
           </div>
           <section>
-            <label htmlFor='password'>Password</label>
-            <input type='password' />
+            <form>
+              {/* <!-- Password input --> */}
+              <div>
+                <div className='relative flex z-0'>
+                  <input
+                    type={fieldType}
+                    name='password'
+                    className={inputStyle}
+                    placeholder='Password'
+                    onChange={handleChange}
+                  />
+                  <label
+                    className='px-2 py-1 text-sm text-red-500 font-mono cursor-pointer absolute right-0'
+                    htmlFor='toggle'
+                  >
+                    <img
+                      onClick={() =>
+                        showPassword(fieldType, setFieldType, setEyeIcon)
+                      }
+                      src={eyeIcon}
+                      className='h-6 w-6 my-2 mr-2'
+                      alt='open eye'
+                    />
+                  </label>
+                </div>
+                <p className='h-4'></p>
+              </div>
 
-             {/* <!-- Submit button --> */}
-        <div className='mb-2'>
-          <SubmitButton
-            loadingAnimation={updateAnimation}
-            mainButtonContent={mainButtonContent}
-            responseMessage={updateResponseMessage}
-            buttonMessage='Update Profile'
-            spinnerHeight='h-5'
-            spinnerWidth='w-5'
-            action={updatePassword}
-          />
-        </div>
+              {/* <!--Confirm Password input --> */}
+              <div>
+                <div className='relative flex z-0'>
+                  <input
+                    type={fieldTypeConfirm}
+                    name='confirmPassword'
+                    className={inputStyle}
+                    placeholder='Confirm Password'
+                    onChange={handleChange}
+                  />
+                  <label
+                    className='px-2 py-1 text-sm text-red-500 font-mono cursor-pointer absolute right-0'
+                    htmlFor='toggle'
+                  >
+                    <img
+                      onClick={() =>
+                        showConfirmPassword(
+                          fieldTypeConfirm,
+                          setFieldTypeConfirm,
+                          setEyeIconConfirm
+                        )
+                      }
+                      src={eyeIconConfirm}
+                      className='h-6 w-6 my-2 mr-2'
+                      alt='open eye'
+                    />
+                  </label>
+                </div>
+                <p className={hiddenPass}>
+                  {formResponses.password === true && (
+                    <span className='text-xs text-green-500 dark:text-green-700 font-medium'>
+                      {formResponses.passwordMessage}
+                    </span>
+                  )}
+                  {formResponses.password === false && (
+                    <span className='text-xs text-red-500 dark:text-red-700 font-medium'>
+                      {formResponses.passwordError}
+                    </span>
+                  )}
+                </p>
+              </div>
+
+              {/* <!-- Submit button --> */}
+              <div className='mb-2'>
+                <SubmitButton
+                  loadingAnimation={updateAnimation}
+                  mainButtonContent={mainButtonContent}
+                  responseMessage={updateResponseMessage}
+                  buttonMessage='Update Profile'
+                  spinnerHeight='h-5'
+                  spinnerWidth='w-5'
+                  action={updatePassword}
+                />
+              </div>
+            </form>
           </section>
-        </div>
+        </main>
       )}
     </div>
   );
--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -26,13 +26,14 @@ function Contact() {
                   <h1 className='text-2xl font-bold'>Contact Me!</h1>
                 </div>
                 <div className='sm:px-6'>
-                  <p className='p-2 md:mx-6 text-center lg:mx-4 leading-5 font-semibold'>If you need anything at all, just get in touch!</p>
+                  <p className='p-2 md:mx-6 text-center lg:mx-4 leading-5 font-semibold'>
+                    If you need anything at all, just get in touch!
+                  </p>
                   <p className='p-2 pb-6 md:mx-6 lg:mx-4 leading-5'>
-                     If you need
-                    quotes or just to talk through some ideas. I am available to
-                    help. Either contact me throught the form provided, or use
-                    one the email address or phone number below. Myself or my
-                    staff are available to help all week.
+                    If you need quotes or just to talk through some ideas. I am
+                    available to help. Either contact me throught the form
+                    provided, or use one the email address or phone number
+                    below. Myself or my staff are available to help all week.
                     <br />
                     If you have designs made through our design tool we can
                     access them and get and up to date view of what site design
@@ -42,67 +43,67 @@ function Contact() {
               </article>
 
               <div className='grid grid-cols-3 mx-1 gap-2'>
-                <article className='grid bg-main-colour border-4 text-white border-purple-700 border-solid rounded-xl text-center px-2 md:px-4 py-4 md:py-8 w-full group cursor-pointer hover:scale-105 hover:text-white hover:outline hover:outline-2 hover:outline-transparent-purple'>
-                  <div className='grid justify-center'>
+                <a
+                  href='mailto:tom@webdesignsbytom.com'
+                  rel='noreferrer'
+                  target='_blank'
+                  className='text-xs md:text-base'
+                >
+                  <article className='grid bg-main-colour border-4 text-white border-purple-700 border-solid rounded-xl text-center px-2 md:px-4 py-4 md:py-8 w-full group cursor-pointer hover:scale-105 hover:text-white hover:outline hover:outline-2 h-full hover:outline-transparent-purple'>
                     <div className='grid justify-center'>
-                      <MdEmail />
-                    </div>
-                    <h4 className='text-sm font-semibold md:text-base'>
-                      Email
-                    </h4>
-                    <h5 className='text-ss md:text-xs break-words overflow-hidden'>
-                      tom@webdesignsbytom.com
-                    </h5>
-                    <a
-                      href='mailto:tom@webdesignsbytom.com'
-                      rel='noreferrer'
-                      target='_blank'
-                      className='text-xs md:text-base'
-                    >
+                      <div className='grid justify-center'>
+                        <MdEmail />
+                      </div>
+                      <h4 className='text-sm font-semibold md:text-base'>
+                        Email
+                      </h4>
+                      <h5 className='text-ss md:text-xs break-words overflow-hidden'>
+                        tom@webdesignsbytom.com
+                      </h5>
                       Send a email
-                    </a>
-                  </div>
-                </article>
-
-                <article className='bg-main-colour rounded-xl border-4 text-white border-yellow-500 border-solid text-center px-2 md:px-4 py-4 md:py-8 w-full group cursor-pointer hover:scale-105 hover:text-white hover:outline hover:outline-2 hover:outline-transparent-yellow'>
-                  <div className='grid justify-center'>
-                    <div className='grid justify-center'>
-                      <GiRaven />
                     </div>
-                    <h4 className='text-sm font-semibold md:text-base'>
-                      Raven
-                    </h4>
-                    <h5 className='text-ss md:text-xs'>webdesignsbytom</h5>
-                    <a
-                      href='http:facebook-subdomain'
-                      rel='noreferrer'
-                      target='_blank'
-                      className='text-xs md:text-base'
-                    >
+                  </article>
+                </a>
+
+                <a
+                  href='http:facebook-subdomain'
+                  rel='noreferrer'
+                  target='_blank'
+                  className='text-xs md:text-base'
+                >
+                  <article className='bg-main-colour rounded-xl border-4 text-white border-yellow-500 border-solid text-center px-2 md:px-4 py-4 md:py-8 w-full group cursor-pointer hover:scale-105 hover:text-white hover:outline hover:outline-2 h-full hover:outline-transparent-yellow'>
+                    <div className='grid justify-center'>
+                      <div className='grid justify-center'>
+                        <GiRaven />
+                      </div>
+                      <h4 className='text-sm font-semibold md:text-base'>
+                        Raven
+                      </h4>
+                      <h5 className='text-ss md:text-xs'>webdesignsbytom</h5>
                       Send a scroll
-                    </a>
-                  </div>
-                </article>
-
-                <article className='bg-main-colour border-4 text-white border-green-700 border-solid rounded-xl text-center px-2 md:px-4 py-4 md:py-8 w-full group cursor-pointer hover:scale-105 hover:text-white hover:outline hover:outline-2 hover:outline-transparent-green'>
-                  <div className='grid justify-center'>
-                    <div className='grid justify-center'>
-                      <ImWhatsapp />
                     </div>
-                    <h4 className='text-sm font-semibold md:text-base'>
-                      WhatsApp
-                    </h4>
-                    <h5 className='text-ss md:text-xs'>Chat any time</h5>
-                    <a
-                      href='https://api.whatsapp.com/send?phone+447541576148'
-                      rel='noreferrer'
-                      target='_blank'
-                      className='text-xs md:text-base'
-                    >
+                  </article>
+                </a>
+
+                <a
+                  href='https://api.whatsapp.com/send?phone+447541576148'
+                  rel='noreferrer'
+                  target='_blank'
+                  className='text-xs md:text-base'
+                >
+                  <article className='bg-main-colour border-4 text-white border-green-700 border-solid rounded-xl text-center px-2 md:px-4 py-4 md:py-8 w-full group cursor-pointer hover:scale-105 hover:text-white hover:outline hover:outline-2 h-full hover:outline-transparent-green'>
+                    <div className='grid justify-center'>
+                      <div className='grid justify-center'>
+                        <ImWhatsapp />
+                      </div>
+                      <h4 className='text-sm font-semibold md:text-base'>
+                        WhatsApp
+                      </h4>
+                      <h5 className='text-ss md:text-xs'>Chat any time</h5>
                       Send a message
-                    </a>
-                  </div>
-                </article>
+                    </div>
+                  </article>
+                </a>
               </div>
             </section>
 
