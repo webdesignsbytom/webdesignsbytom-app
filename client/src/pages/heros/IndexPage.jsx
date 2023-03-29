101a4beaef1316a09d99eaa6cb4285c97fc46006
xtombrock <xtombrock1989@gmail.com>
2023-03-29 04:24:30 +0100

    save

--- client/src/pages/heros/IndexPage.jsx
+++ client/src/pages/heros/IndexPage.jsx
@@ -4,24 +4,47 @@ import PlanetHero from '../../pages/heros/PlanetHero';
 import TimeOfDay from '../../pages/heros/TimeOfDay';
 import NewsletterHero from './NewsletterHero';
 
+const pageList = ['NewsletterHero', 'PlanetHero', 'TimeOfDay'];
+
 function IndexPage() {
   const [pageNumber, setPageNumber] = useState(1);
 
-  console.log('PAGE: ' + pageNumber)
-
   const increasePageNumber = () => {
-    setPageNumber(prev => prev + 1)
-  }
+    if (pageNumber === pageList.length) {
+      setPageNumber(1);
+    } else {
+      setPageNumber((prev) => prev + 1);
+    }
+  };
 
   const decreasePageNumber = () => {
-    setPageNumber(prev => prev - 1)
-  }
+    if (pageNumber === 1) {
+      setPageNumber(pageList.length);
+    } else {
+      setPageNumber((prev) => prev - 1);
+    }
+  };
 
   return (
     <>
-      {(pageNumber === 3) && <NewsletterHero increasePageNumber={increasePageNumber} decreasePageNumber={decreasePageNumber} />}
-      {(pageNumber === 2) && <PlanetHero increasePageNumber={increasePageNumber} decreasePageNumber={decreasePageNumber} />}
-      {(pageNumber === 1) && <TimeOfDay increasePageNumber={increasePageNumber} decreasePageNumber={decreasePageNumber} />}
+      {pageNumber === 3 && (
+        <NewsletterHero
+          increasePageNumber={increasePageNumber}
+          decreasePageNumber={decreasePageNumber}
+        />
+      )}
+      {pageNumber === 2 && (
+        <PlanetHero
+          increasePageNumber={increasePageNumber}
+          decreasePageNumber={decreasePageNumber}
+        />
+      )}
+      {pageNumber === 1 && (
+        <TimeOfDay
+          increasePageNumber={increasePageNumber}
+          decreasePageNumber={decreasePageNumber}
+        />
+      )}
     </>
   );
 }
