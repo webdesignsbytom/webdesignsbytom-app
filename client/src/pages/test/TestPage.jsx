9ff1a6d9b9162ac4764c976562c427e515b0bb7f
xtombrock <xtombrock1989@gmail.com>
2023-03-29 13:04:51 +0100

    save

--- client/src/pages/test/TestPage.jsx
+++ client/src/pages/test/TestPage.jsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 // Icons
 import { HiArrowSmLeft } from 'react-icons/hi';
 import { HiArrowSmRight } from 'react-icons/hi';
-
+import Logo from '../../assets/img/logos/wdbt-white.png'
+import Logo2 from '../../assets/img/logos/image.png'
+import Logo3 from '../../assets/img/logos/WD-02.png'
+import Logo4 from '../../assets/img/logos/WD-04.svg'
 
 function TestPage() {
   return (
     <div className='grid relative bg-black h-[100vh] w-full'>
-      {/* nav */}
-      
+    <img src={Logo4} className='w-20' alt="" />
     </div>
   );
 }
