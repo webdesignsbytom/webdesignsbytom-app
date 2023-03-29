754d2dc752c0153e6ce72ebc24dad608a91e772f
xtombrock <xtombrock1989@gmail.com>
2023-03-29 15:13:48 +0100

    save

--- client/src/components/about/AboutSection.jsx
+++ client/src/components/about/AboutSection.jsx
@@ -12,7 +12,7 @@ function AboutSection() {
               <div className='w-full px-3 sm:px-4 xl:w-1/2'>
                 <div className='py-3 sm:py-4'>
                   <img
-                    src='https://cdn.tailgrids.com/2.0/image/marketing/images/about/about-01/image-1.jpg'
+                    src='https://github.com/webdesignbytom/webdesignsbytom-app/blob/main/client/src/assets/img/sales/wdbt1.png?raw=true'
                     alt=''
                     className='w-full rounded-2xl'
                   />
