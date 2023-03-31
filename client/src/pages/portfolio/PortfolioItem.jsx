e56043ceec97105f82c0666321b716b9d73dab8c
xtombrock <xtombrock1989@gmail.com>
2023-03-31 22:57:01 +0100

    save

--- client/src/pages/portfolio/PortfolioItem.jsx
+++ client/src/pages/portfolio/PortfolioItem.jsx
@@ -90,7 +90,7 @@ function PortfolioItem() {
           </nav>
         </section>
         {/* MAIN PAGE */}
-        <main className='grid lg:grid-cols-xo lg:gap-6 p-4 lg:h-[calc(100vh-64px)] lg:max-h-[calc(100vh-64px)] lg:overflow-hidden'>
+        <main className='grid lg:grid-cols-xo mt-5 lg:mt-0 lg:gap-6 p-4 lg:h-[calc(100vh-64px)] lg:max-h-[calc(100vh-64px)] lg:overflow-hidden'>
           <div className='h-full'>
             <div className='text-3xl mb-4 font-bold'>
               <h1>{portfolioItem.title}</h1>
@@ -101,18 +101,24 @@ function PortfolioItem() {
               {portfolioItem.headline}
             </div>
 
-            <article className=''>{portfolioItem.desc}</article>
+            <article className='bg-gray-100 my-2 py-2 lg:my-0'>
+              <p>{portfolioItem.desc}</p>
+            </article>
 
-            <section className='outline-2 outline-black outline rounded p-1 w-fit'>
+            <section className='outline-2 mt-5 lg:mt-0 outline-black outline rounded p-1 w-full lg:w-fit'>
               <div className=''>
                 <div className='text-center text-xl font-semibold mb-2'>
                   <h3>Skills Used</h3>
                 </div>
 
-                <article className='outline-2 outline-black outline rounded p-1 w-fit'>
+                <article className='outline-2 outline-black outline rounded p-1 w-full lg:w-fit'>
                   <ul className='p-1'>
                     {portfolioItem.skills.map((skill, index) => {
-                      return <li key={index}>{skill}</li>;
+                      console.log('skills', skill)
+                      return <li key={index}>
+                        <h4>{skill}</h4>
+                        <h4>{skill.icon}</h4>
+                      </li>;
                     })}
                   </ul>
                 </article>
@@ -133,8 +139,67 @@ function PortfolioItem() {
                 })}
               </ul>
             </div>
-            {/* Display container */}
-            <section className='grid relative'>
+
+            {/* Display container small */}
+            <section className='grid mt-10 lg:hidden'>
+              {/* MONITOR */}
+              <section className='grid w-full justify-center grid-rows-aa px-2'>
+                <div className='grid pt-2 max-w-[400px] max-h-[290px] pr-2 pl-2 grid-rows-rev bg-black rounded-lg border-2 border-solid border-gray-600'>
+                  <div className='bg-black rounded border-2 overflow-hidden border-solid border-gray-600'>
+                    <img
+                      className='w-full h-full'
+                      src={portfolioItem.images[2]}
+                      alt='phone display'
+                    />
+                  </div>
+                  <div className='grid grid-flow-col items-center py-1 pr-4 gap-2 justify-end'>
+                    <div className='border-2 border-solid border-gray-600 rounded-full bg-white h-[10px] w-[10px]'></div>
+                    <div className='border-2 border-solid border-gray-600 rounded-full bg-white h-[10px] w-[10px]'></div>
+                    <div className='border-2 border-solid border-gray-600 rounded-full bg-white h-[10px] w-[10px]'></div>
+                    <div className='border-2 border-solid border-gray-600 rounded-full bg-white h-[10px] w-[10px]'></div>
+                  </div>
+                </div>
+              </section>
+
+              {/* LAPTOP */}
+              <section className='grid grid-rows-ten-one -mt-32 h-fit px-4 justify-center items-center'>
+                <div className='grid items-center justify-center'>
+                  <div className='grid p-1 pt-3 bg-black rounded-tr-lg rounded-tl-lg border-2 border-solid border-gray-600 max-w-[300px]'>
+                    <div className='border-2 border-solid h-full border-gray-600 overflow-hidden bg-black'>
+                      <img
+                        className='h-full w-full'
+                        src={portfolioItem.images[1]}
+                        alt='laptop display'
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className='grid min-h-[15px] items-center grid-cols-reg pl-8 rounded-tl-sm rounded-tr-sm rounded-bl-lg rounded-br-lg bg-black border-2 border-solid border-gray-600'>
+                  <div className='grid grid-flow-col gap-2'>
+                    <div className='h-[10px] w-[10px] border-2 border-solid border-gray-600 rounded-full bg-black'></div>
+                    <div className='h-[10px] w-[10px] border-2 border-solid border-gray-600 rounded-full bg-black'></div>
+                  </div>
+                </div>
+              </section>
+              {/* PHONE */}
+              <section className='grid -mt-44 relative z-30 w-full items-center justify-center'>
+                <div className='bg-black grid h-[160px] w-[88px] rounded-lg p-1 border-2 border-solid border-gray-600'>
+                  <div className='bg-gray-600 relative overflow-hidden rounded-lg border-2 border-solid border-gray-600'>
+                    <img
+                      className='w-full h-full'
+                      src={portfolioItem.images[0]}
+                      alt='phone display'
+                    />
+                    <div className='grid absolute justify-center bottom-1 w-full'>
+                      <div className='h-[15px] w-[15px] border-2 border-solid border-gray-600 rounded-full'></div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </section>
+
+            {/* Display container large */}
+            <section className='hidden lg:grid relative'>
               {/* PHONE */}
               <section className='grid z-30 absolute bottom-2 left-4 items-center justify-center'>
                 <div className='relative bg-white grid h-[160px] w-[88px] rounded-lg p-1 border-2 border-solid border-black'>
@@ -174,7 +239,7 @@ function PortfolioItem() {
 
               {/* MONITOR */}
               <section className='grid grid-rows-aa absolute top-1 right-1'>
-                <div className='grid w-[300px] h-[220px] lg:w-[400px] lg:h-[290px] pt-2 pr-2 pl-2 grid-rows-rev bg-black rounded-lg border-2 border-solid border-gray-600'>
+                <div className='grid lg:w-[400px] lg:h-[290px] pt-2 pr-2 pl-2 grid-rows-rev bg-black rounded-lg border-2 border-solid border-gray-600'>
                   <div className='bg-black rounded border-2 overflow-hidden border-solid border-gray-600'>
                     <img
                       className='w-full h-full'
